0612ae0b7c98d2a8c7c010acf08d6b6cf2d71e80
Sh1ny4 <60614398+Sh1ny4@users.noreply.github.com>
2026-01-01 17:20:40 +0100

    initial perk removal test

---
+++ TestingMod/patches/EarlyWipStuff/PerksPatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem.CharacterDevelopment;
+
+namespace TestingMod.patches.EarlyWipStuff
+{
+    [HarmonyPatch(typeof(DefaultPerks), "InitializeAll")]
+    internal class PerksPatch
+    {
+        [HarmonyPrefix]
+        static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
